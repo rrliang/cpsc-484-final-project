003c8cc1ecd8b3f7ea826912490075b1b302a851
Noah Dee <noah@Noahs-MBP-5.stdusr.yale.internal>
2024-04-07 15:57:41 -0400

    connect to port

---
+++ script.js
@@ -0,0 +1,22 @@
+var host = "cpsc484-01.yale.internal:8888";
+
+$(document).ready(function() {
+  frames.start();
+});
+
+var frames = {
+  socket: null,
+
+  start: function() {
+    var url = "ws://" + host + "/frames";
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function(event) {
+      frames.show(JSON.parse(event.data));
+    };
+  },
+
+  show: function(frame) {
+    console.log(frame);
+  }
+};
+console.log("hgello");
